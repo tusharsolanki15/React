f7c9e49a14d6735a4fb9bb8d25290e7073ec23f9
Tushar <tusharsolanki2001152@gmail.com>
2023-07-12 17:49:04 +0530

    module files

---
+++ src/module1.mjs
@@ -0,0 +1,5 @@
+import name,{a,c,d} from './module2.mjs'
+console.log(name)
+console.log(a)
+console.log(c)
+console.log(d)
---
+++ src/module2.mjs
@@ -0,0 +1,9 @@
+const a = "sachin"
+const b = "Raj"
+const c = "Harry"
+const d = "Ram"
+
+export default b;
+export {a};
+export {c};
+export {d};
